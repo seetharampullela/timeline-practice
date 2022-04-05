7a78c494ffcdaea726dc53bb8fa6a457aef122bd
seetharampullela <seetharampullela@gmail.com>
2022-04-05 04:36:14 +0000

    second commit

--- src/components/TimelineView/index.js
+++ src/components/TimelineView/index.js
@@ -7,34 +7,27 @@ import ProjectTimeLineCard from '../ProjectTimelineCard'
 const TimeLineView = props => {
   const {timelineItemsList} = props
 
-  const courseItemsList = timelineItemsList.filter(
-    each => each.categoryId === 'COURSE',
-  )
+  //   const courseItemsList = timelineItemsList.filter(
+  //     each => each.categoryId === 'COURSE',
+  //   )
 
-  const projectItemsList = timelineItemsList.filter(
-    each => each.categoryId === 'PROJECT',
-  )
-  console.log(projectItemsList)
+  //   const projectItemsList = timelineItemsList.filter(
+  //     each => each.categoryId === 'PROJECT',
+  //   )
+  //   console.log(projectItemsList)
 
-  //   const courseItems = timelineItemsList.map(each => ({
-  //     title: each.title,
-  //   }))
+  const renderTimelineCard = item => {
+    if (item.categoryId === 'PROJECT') {
+      return <ProjectTimeLineCard key={item.id} projectItem={item} />
+    }
+    return <CourseTimeLineCard key={item.id} courseItem={item} />
+  }
 
   return (
     <div>
       <h1>MY JOURNEY OF CCBP 4.0</h1>
       <Chrono mode="VERTICAL_ALTERNATING" items={timelineItemsList}>
-        {courseItemsList.map(each => (
-          <div>
-            <CourseTimeLineCard courseItem={each} key={each.id} />
-          </div>
-        ))}
-
-        {projectItemsList.map(each => (
-          <div>
-            <ProjectTimeLineCard projectItem={each} key={each.id} />
-          </div>
-        ))}
+        {timelineItemsList.map(eachItem => renderTimelineCard(eachItem))}
       </Chrono>
     </div>
   )
